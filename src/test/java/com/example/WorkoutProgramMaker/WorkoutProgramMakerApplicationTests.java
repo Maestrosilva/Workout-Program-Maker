5c915fb949ea331998b491121ef5c6974492a4d2
Georgi <116490071+Maestrosilva@users.noreply.github.com>
2024-03-16 15:22:33 +0200

    Add files via upload

---
+++ src/test/java/com/example/WorkoutProgramMaker/WorkoutProgramMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkoutProgramMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutProgramMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
